b12527ace6dce277187508117ddc9e63c9eb1cfc
msuraj11 <suraj.mamidi@gmail.com>
2025-01-02 19:26:36 +0100

    starting React v19 forwardRef is not required and will be depricated in future.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,72 +1,66 @@
-import {forwardRef, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 
-const Player = forwardRef(
-  (
-    {
-      savedName,
-      symbol,
-      isActive,
-      onSaveName,
-      freshGame,
-      hasInvalidInput,
-      hasEmptyInputs
-    },
-    ref
-  ) => {
-    const [isEditing, setEditing] = useState(true);
-    const [playerName, setPlayerName] = useState(savedName);
-    const invalidClassName = hasInvalidInput ? 'invalid' : undefined;
-    const spanClassName = `player-name${
-      invalidClassName ? ` ${invalidClassName}` : ''
-    }`;
-    const readyToPlay = !hasInvalidInput && !hasEmptyInputs && isActive;
+export default function Player({
+  savedName,
+  symbol,
+  isActive,
+  onSaveName,
+  freshGame,
+  hasInvalidInput,
+  hasEmptyInputs,
+  ref // starting React v19 forwardRef is not required and will be depricated in future.
+}) {
+  const [isEditing, setEditing] = useState(true);
+  const [playerName, setPlayerName] = useState(savedName);
+  const invalidClassName = hasInvalidInput ? 'invalid' : undefined;
+  const spanClassName = `player-name${
+    invalidClassName ? ` ${invalidClassName}` : ''
+  }`;
+  const readyToPlay = !hasInvalidInput && !hasEmptyInputs && isActive;
 
-    useEffect(() => {
-      if (hasInvalidInput) {
-        setEditing(true);
-        setPlayerName(savedName);
-      }
-    }, [hasInvalidInput]);
-
-    function handleEditClick() {
-      setEditing((prevState) => !prevState);
-      if (isEditing) {
-        onSaveName(symbol, playerName);
-      }
+  useEffect(() => {
+    if (hasInvalidInput) {
+      setEditing(true);
+      setPlayerName(savedName);
     }
+  }, [hasInvalidInput]);
 
-    function handlePlayerNameChange(event) {
-      setPlayerName(event.target.value?.toLowerCase().trim());
+  function handleEditClick() {
+    setEditing((prevState) => !prevState);
+    if (isEditing) {
+      onSaveName(symbol, playerName);
     }
+  }
 
-    let playerNameJsx = <span className={spanClassName}>{savedName}</span>;
-    if (isEditing && freshGame) {
-      playerNameJsx = (
-        <input
-          type="text"
-          required
-          onChange={handlePlayerNameChange}
-          value={playerName}
-          className={invalidClassName}
-          ref={ref}
-        />
-      );
-    }
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value?.toLowerCase().trim());
+  }
 
-    return (
-      <li className={readyToPlay ? 'active' : undefined}>
-        <span className="player">
-          {playerNameJsx}
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        {freshGame && (
-          <button onClick={handleEditClick} disabled={!playerName}>
-            {isEditing ? 'Save' : 'Edit'}
-          </button>
-        )}
-      </li>
+  let playerNameJsx = <span className={spanClassName}>{savedName}</span>;
+  if (isEditing && freshGame) {
+    playerNameJsx = (
+      <input
+        type="text"
+        required
+        onChange={handlePlayerNameChange}
+        value={playerName}
+        className={invalidClassName}
+        ref={ref}
+      />
     );
   }
-);
 
-export default Player;
+  return (
+    <li className={readyToPlay ? 'active' : undefined}>
+      <span className="player">
+        {playerNameJsx}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      {freshGame && (
+        <button onClick={handleEditClick} disabled={!playerName}>
+          {isEditing ? 'Save' : 'Edit'}
+        </button>
+      )}
+    </li>
+  );
+}
